af00d3eea1e3532f26b597aab9b98adac9fe4990
fdlhhmd <d20221101818@siswa.upsi.edu.my>
2025-05-21 08:31:48 +0800

    adjust pagination

--- app/Livewire/ProductIndex.php
+++ app/Livewire/ProductIndex.php
@@ -11,7 +11,7 @@ class ProductIndex extends Component
     public function render()
     {
         return view('livewire.product-index', [
-            'products' => Product::paginate(10),
+            'products' => Product::paginate(2),
         ]);
     }
 }
