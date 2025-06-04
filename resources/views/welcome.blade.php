3f28ee55cc14b8fabe58c621a65741438a7d903c
fdlhhmd <d20221101818@siswa.upsi.edu.my>
2025-06-04 12:09:14 +0800

    hide about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,11 +66,11 @@
                             Dashboard
                         </a>
                     @else
-                        <a 
+                        {{-- <a 
                             href="{{ route('about') }}" 
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
                             About
-                        </a>
+                        </a> --}}
                     
                         <a
                             href="{{ route('login') }}"
